ae71a2f70d78f1429d9f2a826d44e439a94f5475
s17858 <s17858@pjwstk.edu.pl>
2024-04-12 21:32:29 +0200

    Implementacja metody ObliczSrednia

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace APBD;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Commit 1");
-Console.WriteLine("Commit 2");
-Console.WriteLine("Commit 3");
+public class Program
+{
+
+    public static double ObliczSrednia(int[] tablica)
+    {
+        int suma = 0;
+        foreach (int liczba in tablica)
+        {
+            suma += liczba;
+        }
+
+        double srednia = (double)suma / tablica.Length;
+        return srednia;
+    }
+
+    public static void Main()
+    {
+        int[] tablica = {1, 2, 3, 4, 5 };
+
+    double srednia = ObliczSrednia(tablica);
+        Console.WriteLine($"Srednia: {srednia:F2}");
+    }
+}
